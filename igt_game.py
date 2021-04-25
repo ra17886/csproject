f348b2ade6220039ae32a0fd39822a4a71c0404c
Roshan Ark <roshanark@Roshans-MacBook-Pro.local>
2021-04-25 13:55:32 +0100

    vse added

--- igt_game.py
+++ igt_game.py
@@ -21,3 +21,22 @@ def drawPrize(box):
         if n<0.1: reward -=250
     
     return reward
+
+def vse_drawPrize(box):
+    n = np.random.rand()
+    reward = 0
+    loss = 0
+    if box == 0:
+        reward += 100
+        if n<0.5: loss -=250
+    elif box == 1:
+        reward += 100
+        if n<0.1: loss -=1250
+    elif box ==2:
+        reward += 50
+        if n<0.5: loss -=50
+    elif box ==3:
+        reward +=50
+        if n<0.1: loss -=250
+    
+    return reward, loss
